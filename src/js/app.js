5327d940cfe9fb9a6c254442588290f5a88c1c92
mhaerden90 <mhaerden@gmail.com>
2022-02-14 00:54:59 +0100

    Removed toWei function

--- src/js/app.js
+++ src/js/app.js
@@ -202,7 +202,6 @@ const App = {
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function(instance) {
-            const productPrice = web3.toWei(1, "ether");
             console.log('productPrice',productPrice);
             return instance.sellItem(App.upc, App.productPrice, {from: App.metamaskAccountID});
         }).then(function(result) {
